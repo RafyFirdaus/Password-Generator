4781b6172ce761e535f026a62101c8fff3b3f4d7
MHD Rafy Firdaus <163707529+RafyFirdaus@users.noreply.github.com>
2024-11-14 23:42:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+export const generatePassword = (length, options) => {
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const specialChars = "!@#$%^&*()";
+
+    let characterSet = '';
+    if (options.includeUppercase) characterSet += uppercase;
+    if (options.includeLowercase) characterSet += lowercase;
+    if (options.includeNumbers) characterSet += numbers;
+    if (options.includeSpecialChars) characterSet += specialChars;
+
+    if (characterSet === '') {
+        throw new Error('At least one character type must be selected.');
+    }
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characterSet.length);
+        password += characterSet[randomIndex];
+    }
+
+    return password;
+};
